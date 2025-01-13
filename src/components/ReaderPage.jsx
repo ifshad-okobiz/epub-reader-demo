a8b3c1de1039b0bad376e8a92d1c5a08bf48cf48
ifshad-okobiz <ifshad.okobiz@gmail.com>
2025-01-13 18:51:38 +0600

    title added

--- src/components/ReaderPage.jsx
+++ src/components/ReaderPage.jsx
@@ -6,6 +6,7 @@ export const ReaderPage = () => {
   return (
     <div style={{ height: "100vh" }}>
       <ReactReader
+        title="New Book"
         url="https://react-reader.metabits.no/files/alice.epub"
         location={location}
         locationChanged={(epubcfi) => setLocation(epubcfi)}
