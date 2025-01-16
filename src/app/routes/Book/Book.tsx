8ab460c93816259ec0942260760db7b72e7aae34
ifshad-okobiz <ifshad.okobiz@gmail.com>
2025-01-16 11:30:14 +0600

    books added

--- src/app/routes/Book/Book.tsx
+++ src/app/routes/Book/Book.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useLocation, useParams } from "react-router";
 
 function Book() {
-  return <div>Book</div>;
+  const { state } = useLocation();
+  const { id } = useParams();
+
+  const book = state?.book;
+
+  if (!book) {
+    return <p>Book data not found. Please go back and select a book.</p>;
+  }
+
+  return (
+    <div>
+      <h1>Reading: {book.name}</h1>
+      <p>Title: {book.title}</p>
+      <p>ID: {id}</p>
+    </div>
+  );
 }
 
 export default Book;
--- src/app/routes/Books/Books.tsx
+++ src/app/routes/Books/Books.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { ReaderPage } from "../../../components/ReaderPage";
+import { useNavigate } from "react-router";
 
 function Books() {
+  const [books, setBooks] = React.useState([]);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/books")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+  console.log(books);
+
+  const handleBookClick = (book) => {
+    navigate(`/books/${book._id}`, { state: book });
+  };
+
   return (
-    <div>
-      <ReaderPage />
+    <div className="w-1/2 mx-auto mt-5">
+      <h1 className="text-2xl font-semibold mb-5">Available Books</h1>
+      <div className="grid grid-cols-3 gap-3">
+        {books.map((book, i) => (
+          <div key={i} className="bg-rose-100 rounded shadow-md p-4">
+            <h1 className="font-medium">{book.name}</h1>
+            <p className="text-opacity-70">{book.title}</p>
+            <button onClick={() => handleBookClick(book)}>Read</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
